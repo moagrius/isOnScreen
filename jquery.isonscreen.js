06ecbec51e2ba94bf7eb4218e65d992df5e40c6e
Mike Dunn <mdunn@thedailydot.com>
2017-02-04 22:16:02 -0600

    update to callback version

--- jquery.isonscreen.js
+++ jquery.isonscreen.js
@@ -1,45 +1,52 @@
 (function ($) {
 
-    $.fn.isOnScreen = function(x, y){
+  /**
+  * Tests if a node is positioned within the current viewport.
+  * It does not test any other type of "visibility", like css display,
+  * opacity, presence in the dom, etc - it only considers position.
+  * 
+  * By default, it tests if at least 1 pixel is showing, regardless of
+  * orientation - however an optional argument is accepted, a callback
+  * that is passed the number of pixels visible on each edge - the return
+  * (true of false) of that callback is used instead.
+  */
+  $.fn.isOnScreen = function(test){
 
-        if(x == null || typeof x == 'undefined') x = 1;
-        if(y == null || typeof y == 'undefined') y = 1;
+    var height = this.outerHeight();
+    var width = this.outerWidth();
 
-        var win = $(window);
+    if(!width || !height){
+      return false;
+    }
+    
+    var win = $(window);
 
-        var viewport = {
-            top : win.scrollTop(),
-            left : win.scrollLeft()
-        };
-        viewport.right = viewport.left + win.width();
-        viewport.bottom = viewport.top + win.height();
-
-        var height = this.outerHeight();
-        var width = this.outerWidth();
-
-        if(!width || !height){
-            return false;
-        }
-
-        var bounds = this.offset();
-        bounds.right = bounds.left + width;
-        bounds.bottom = bounds.top + height;
-
-        var visible = (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
-
-        if(!visible){
-            return false;
-        }
-
-        var deltas = {
-            top : Math.min( 1, ( bounds.bottom - viewport.top ) / height),
-            bottom : Math.min(1, ( viewport.bottom - bounds.top ) / height),
-            left : Math.min(1, ( bounds.right - viewport.left ) / width),
-            right : Math.min(1, ( viewport.right - bounds.left ) / width)
-        };
-
-        return (deltas.left * deltas.right) >= x && (deltas.top * deltas.bottom) >= y;
-
+    var viewport = {
+      top : win.scrollTop(),
+      left : win.scrollLeft()
     };
+    viewport.right = viewport.left + win.width();
+    viewport.bottom = viewport.top + win.height();
+
+    var bounds = this.offset();
+    bounds.right = bounds.left + width;
+    bounds.bottom = bounds.top + height;
+    
+    var showing = {
+      top : viewport.bottom - bounds.top,
+      left: viewport.right - bounds.left,
+      bottom: bounds.bottom - viewport.top,
+      right: bounds.right - viewport.left
+    };
+
+    if(typeof test == 'function') {
+      return test(showing);
+    }
+    
+    return showing.top > 0
+      && showing.left > 0
+      && showing.right > 0
+      && showing.bottom > 0;
+  };
 
 })(jQuery);
